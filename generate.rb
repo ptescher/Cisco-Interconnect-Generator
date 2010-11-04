3fdc488aa76a86df0611db1ac0d84444ce3710c0
Patrick Tescher <ptescher@z7networks.com>
2010-11-04 15:13:30 -0700

    Start parsing CSV

--- generate.rb
+++ generate.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/ruby
+require 'csv'
+
+## Configuragion
+
+@FirstInterconnectVlan = 3999
+@CountDown = true
+@Devices = ['Router','Switch','Other']
+
+@DeviceConfigs = ['','','']
+
+CSV.open('input.csv', 'r', ';') do |row|
+ p row
+end
